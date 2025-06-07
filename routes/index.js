2cddea5c53f6d3bc0d610773a6afadfdc13692b4
rawwyurr <roger18gm@gmail.com>
2025-06-07 09:39:32 -0600

    add new routers

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import { getData } from "../controllers/index.js";
 import projectsRouter from './projects.js';
 import jobsRouter from './jobs.js';
+import certificationsRouter from './certifications.js';
+import organizationsRouter from './organizations.js';
 import swaggerRouter from "./swagger.js";
 
 const router = express.Router();
@@ -9,6 +11,8 @@ const router = express.Router();
 router.get("/", getData);
 router.use("/projects", projectsRouter);
 router.use("/jobs", jobsRouter);
+router.use("/certifications", certificationsRouter);
+router.use("/organizations", organizationsRouter);
 router.use("/api-docs", swaggerRouter);
 
 
